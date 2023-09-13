f594546985278a7c29c9eddcd36cbe8adb8270af
kwena <kwenakomape2@gmail.com>
2023-09-13 16:44:29 +0200

    added drop down menu for lessons

---
+++ public/CollaspseMenu.js
@@ -0,0 +1,17 @@
+
+//var coll = document.getElementById(lessons);
+var coll = document.querySelectorAll("#lessons");
+var i;
+// sconsole.log(coll.length);
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    // this.classList.toggle("active");
+    
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
